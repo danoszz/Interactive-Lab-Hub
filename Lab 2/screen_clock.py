6b309971c91f20f4505e250c9c2063f3cee5a0a2
Daan van der Zwaag <daan@devign.it>
2021-09-20 23:45:35 -0400

    push from raspberry pi

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -2,7 +2,7 @@ import time
 import subprocess
 import digitalio
 import board
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import adafruit_rgb_display.st7789 as st7789
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
@@ -31,18 +31,31 @@ disp = st7789.ST7789(
 
 # Create blank image for drawing.
 # Make sure to create image with mode 'RGB' for full color.
-height = disp.width  # we swap height/width to rotate it to landscape!
-width = disp.height
+height = disp.height
+width = disp.width
 image = Image.new("RGB", (width, height))
 rotation = 90
+
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
 # Draw a black filled box to clear the image.
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-disp.image(image, rotation)
-# Draw some shapes.
+
+image = Image.open("candle-3.jpg")
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+# Display image.
+disp.image(image)
+
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+
+#Draw some shapes.
 # First define some constants to allow easy resizing of shapes.
 padding = -2
 top = padding
@@ -50,22 +63,35 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 
 # Turn on the backlight
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+def resizeCandle(img, h):
+    
+    counter = 0;
+    # clear image
+    draw.rectangle((0, 0, 240, 135), outline=0, fill=120)
+    
+    #img = img.copy()
+    #img.paste(img, (0, h // 2))
+    
+   # img = img.transform(img.size, Image.EXTENT, (0, h, 0, 0))
+    img = img.resize((width, h))
+    
+   # img = img.crop((10, 20, 20, 20))
+    #img.filter(ImageFilter.MinFilter)
+    disp.image(img)
 
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
-    # Display image.
-    disp.image(image, rotation)
-    time.sleep(1)
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        resizeCandle(image, 240)
+    if buttonA.value and not buttonB.value:
+        resizeCandle(image, 80)
+    time.sleep(0.1)
