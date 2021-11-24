f682b25d37f451ada1fbe88b569fa8c1ddb3923a
Daan van der Zwaag <daan@devign.it>
2021-11-24 13:12:00 -0500

    fix: namespacing

--- Lab 3/hello-numbers.sh
+++ Lab 3/hello-numbers.sh
@@ -5,4 +5,4 @@
 echo "What year were you born?" | festival --tts
 
 arecord -D hw:2,0 -f cd -c1 -r 48000 -d 3 -t wav recorded_mono-hello-numbers.wav
-python3 get_numbers.py recorded_mono-hello-numbers.wav
+python3 numbers.py recorded_mono-hello-numbers.wav
