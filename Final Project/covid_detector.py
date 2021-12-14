4742a833cb97e6444bd2ce41ca443492b2f024f0
Daan van der Zwaag <daan@devign.it>
2021-12-13 23:57:10 -0500

    fix: merge files

--- Final Project/covid_detector.py
+++ Final Project/covid_detector.py
@@ -5,10 +5,15 @@ import busio
 import board
 import time
 from adafruit_bus_device.i2c_device import I2CDevice
-from struct import pack, unpack
-
+import tensorflow.keras
+import numpy as np
+import cv2
+import sys
+import numpy as np
+import tensorflow as tf
 
 # 1. import helper functions from other labs
+
 from helper_image import displayImage 
 from helper_screen import displayText 
 
@@ -49,22 +54,121 @@ write_register(device, 0x19, 0)
 
 
 # register: touch sensor
-# register: screen
-# register: microphone
-# register: camera
+# TODO: implement proximity sensor, instead of mock data
 
-
+getInputTouchSensor = 255
 
 # 2. load API and externals
 
 # load: Teachable machines
+
+# AUDIO
+
+# Example from Tensorsflow python website
+# source: https://www.tensorflow.org/lite/guide/inference#load_and_run_a_model_in_python
+
+# TODO: connect to microphone
+
+img = tf.placeholder(name="img", dtype=tf.float32, shape=(1, 64, 64, 3))
+const = tf.constant([1., 2., 3.]) + tf.constant([1., 4., 4.])
+val = img + const
+out = tf.identity(val, name="out")
+
+# Convert to TF Lite format
+with tf.Session() as sess:
+  converter = tf.lite.TFLiteConverter.from_session(sess, [img], [out])
+  tflite_model = converter.convert()
+
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_content=tflite_model)
+interpreter.allocate_tensors()
+
+# Continue to get tensors and so forth, as shown above...
+
+# This example is directly copied from the Tensorflow examples provided from the Teachable Machine.
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+img = None
+webCam = False
+if(len(sys.argv)>1 and not sys.argv[-1]== "noWindow"):
+   try:
+      print("I'll try to read your image");
+      img = cv2.imread(sys.argv[1])
+      if img is None:
+         print("Failed to load image file:", sys.argv[1])
+   except:
+      print("Failed to load the image are you sure that:", sys.argv[1],"is a path to an image?")
+else:
+   try:
+      print("Trying to open the Webcam.")
+      cap = cv2.VideoCapture(0)
+      if cap is None or not cap.isOpened():
+         raise("No camera")
+      webCam = True
+   except:
+      print("Unable to access webcam.")
+
+
+# Load the model
+model = tensorflow.keras.models.load_model('/models/keras_model.h5')
+# Load Labels:ds
+labels=[]
+f = open("labels.txt", "r")
+for line in f.readlines():
+    if(len(line)<1):
+        continue
+    labels.append(line.split(' ')[1].strip())
+
+
+while(True):
+    if webCam:
+        ret, img = cap.read()
+
+    rows, cols, channels = img.shape
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    size = (224, 224)
+    img =  cv2.resize(img, size, interpolation = cv2.INTER_AREA)
+    #turn the image into a numpy array
+    image_array = np.asarray(img)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    print("I think its a:",labels[np.argmax(prediction)])
+
+    if webCam:
+        if sys.argv[-1] == "noWindow":
+           cv2.imwrite('detected_out.jpg',img)
+           continue
+        cv2.imshow('detected (press q to quit)',img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            break
+    else:
+        break
+
+cv2.imwrite('detected_out.jpg',img)
+cv2.destroyAllWindows()
+
 # load: webserver 
+# TODO: feature removed due to complexity
+
+#### This where the fun starts! ####
 
 # 3. build interaction flow
 
+# mock data for prototyping purposes
+
 userCoughs = True
-userInView = False
-getInputTouchSensor = 255
+userInView = False or webCam
+
 
 ##### Helper functions: standalone functions to support main flow (and to be cleaner) #####
 
